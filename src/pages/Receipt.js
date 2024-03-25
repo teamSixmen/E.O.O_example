92da4c730293aa0f7352d2f5fa1f2631cc6ef484
sunskwon <sunskwon@gmail.com>
2024-03-25 12:18:46 +0900

    [feature_development]kwon_timer

--- src/pages/Receipt.js
+++ src/pages/Receipt.js
@@ -34,15 +34,15 @@ function Receipt({here, setHere, selectedItems, setSelectedItems, waitingNum, se
 
     useEffect (
         () => {
-            const timer = setInterval(() => {
-                navigate("/");
-            }, 60000);
+            // const timer = setInterval(() => {
+            //     navigate("/");
+            // }, 60000);
             
 
             return () => {
                 setWaitingNum(waitingNum + 1);
-                clearInterval(timer);
-                setSelectedItems([]);
+                // clearInterval(timer);
+                // setSelectedItems([]);
             }
         },
         []
@@ -79,7 +79,7 @@ function Receipt({here, setHere, selectedItems, setSelectedItems, waitingNum, se
                     <h2>합계 금액 : {parseInt(totalPrice / 1000)},{(totalPrice % 1000)? totalPrice % 1000:"000"}</h2>
                     <h3>{here? "매장":"포장"}</h3>
                     <h2>대기순번 : {waitingNum} </h2>
-                    <br/>
+                    <Timer/>
                     <div 
                         className={style.Home}
                     ><h3>화면을 터치하세요!</h3></div>
