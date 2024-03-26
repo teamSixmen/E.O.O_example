97df095636839c85ac42deb1c4ca89218424911b
ChanWool Lim <cksdnf90@gmail.com>
2024-03-26 09:33:55 +0900

    initial commit
    #13
    
    # conflicts resolving

--- src/components/MenuBlock.js
+++ src/components/MenuBlock.js
@@ -2,46 +2,46 @@ import style from "./MenuBlock.module.css";
 
 function MenuBlock({ item, selectedItems, setSelectedItems, change,setChange }) {
 
-    const onClickHandler = () => {
+    // const onClickHandler = () => {
 
-        const index = selectedItems.findIndex(function(selected){return selected.menuCode === item.menuCode});
+    //     const index = selectedItems.findIndex(function(selected){return selected.menuCode === item.menuCode});
 
-        if (index !== -1) {
-            // console.log("plus");
-            let copiedItems = selectedItems;
-            copiedItems[index].quantity += 1;
-            setSelectedItems(copiedItems);
-            setChange(!change);
-            // console.log(selectedItems);
-        } else {
-            if (selectedItems.length < 6) {
-                // console.log("add");
-                const changedItems = [...selectedItems,
-                    {"menuCode": item.menuCode, 
-                     "menuName": item.menuName, 
-                     "price": item.price,
-                     "quantity": 1}];
-                setSelectedItems(changedItems);
-                setChange(!change);
-                // console.log(selectedItems);
-            } else {
-                alert("shopping list is full!");
-            }
-        }
-    };
+    //     if (index !== -1) {
+    //         // console.log("plus");
+    //         let copiedItems = selectedItems;
+    //         copiedItems[index].quantity += 1;
+    //         setSelectedItems(copiedItems);
+    //         setChange(!change);
+    //         // console.log(selectedItems);
+    //     } else {
+    //         if (selectedItems.length < 6) {
+    //             // console.log("add");
+    //             const changedItems = [...selectedItems,
+    //                 {"menuCode": item.menuCode, 
+    //                  "menuName": item.menuName, 
+    //                  "price": item.price,
+    //                  "quantity": 1}];
+    //             setSelectedItems(changedItems);
+    //             setChange(!change);
+    //             // console.log(selectedItems);
+    //         } else {
+    //             alert("shopping list is full!");
+    //         }
+    //     }
+    // };
 
-    return (
-        <>
-            <div
-                onClick={onClickHandler}
-                className={style.Block}
-            >
-                <br/><img src="/images/temp.jpg" width="100px"/>
-                <br/>{item.menuName}<br/>
-                {parseInt(item.price / 1000)},{(item.price % 1000)?item.price % 1000:"000"}원
-            </div>
-        </>
-    );
+    // return (
+    //     <>
+    //         <div
+    //             onClick={() => setModalOpen(true)}
+    //             className={style.Block}
+    //         >
+    //             <br/><img src="/images/temp.jpg" width="100px"/>
+    //             <br/>{item.menuName}<br/>
+    //             {parseInt(item.price / 1000)},{(item.price % 1000)?item.price % 1000:"000"}원
+    //         </div>
+    //     </>
+    // );
 }
 
 export default MenuBlock;
