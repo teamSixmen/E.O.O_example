2c2d1e6e8845717862d70a5494c3167a1f947032
김만호 <krossjsnet88@gmail.com>
2024-03-27 10:52:15 +0900

    Footer 진행바 적용 및 카드삽입 이미지 추가

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -25,7 +25,7 @@ function Card() {
         <>
             <div className={style.Back}>
                 <div className={style.CardBlank}></div>
-                <img src="/images/temp.jpg" width="400px"/>
+                <img src="/images/카드삽입.gif" width="400px" height="400px"/>
                 <h1>카드를 넣어주세요</h1>
                 <h1>카드 결제를 진행합니다.</h1>
             </div>
